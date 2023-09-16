b3de3b67ea1427198cf9c4108f576275d2dc7d33
Mitchell Culligan <mitchellculligan@Mitchells-MacBook-Pro.local>
2023-09-16 18:17:22 -0400

    control functionality implemented/requires testing

--- cav-autera-software/cav-controls/CavControls.py
+++ cav-autera-software/cav-controls/CavControls.py
@@ -1,3 +1,4 @@
+import numpy as np
 class CavControls:
     
     targetVel_mps = 0
@@ -7,7 +8,8 @@ class CavControls:
     decelPdl_pct = 0
     
     def __init__(self):
-        pass
+        self.intergral_err=0.0
+        self.prev_err=0.0
     
     def setTargetVel_mps(self, vel): self.targetVel_mps = vel
     def setEgoVehXVel(self, egoVehXVel_mps): self.egoVehXVel_mps = egoVehXVel_mps
@@ -16,12 +18,22 @@ class CavControls:
     def getDecelPdl_pct(self): return self.decelPdl_pct
     
     def executeLongitudinalControl(self):
-        
+        MIN_STEP=0.01
+
+        kp=0.5
+        ki=23
+        kd=-0.1
+
+        pe=self.targetVel_mps-self.egoVehXVel_mps
+        self.intergral_err+=MIN_STEP*pe
+        de=pe-self.prev_err
+        self.prev_err=pe
+        v=kp*pe+self.intergral_err*ki+kd*de
         # WRITE AN ALGORITHM THAT USES THE TARGET VELOCITY TO GENERATE AN ACCEL/BRAKE PEDAL POSITION TO CONTROL THE VEHICLE
         # IMLPEMENT YOUR CODE HERE! 
-        
-        accelPdl_pct = 0 # accelerator pedal percentage in range [0, 1]
-        decelPdl_pct = 0 # brake pedal percentage in range [0, 1]
+       
+        accelPdl_pct = v # accelerator pedal percentage in range [0, 1]
+        decelPdl_pct = 1-accelPdl_pct # brake pedal percentage in range [0, 1]
         
         self.accelPdl_pct = accelPdl_pct
         self.decelPdl_pct = decelPdl_pct
