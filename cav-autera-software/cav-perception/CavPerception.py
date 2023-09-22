0f2e555c133ae4c2e645dee6465324ea5885b348
Mitchell Culligan <mitchellculligan@Mitchells-MacBook-Pro.local>
2023-09-21 22:00:35 -0400

    clustering perception functionality added

--- cav-autera-software/cav-perception/CavPerception.py
+++ cav-autera-software/cav-perception/CavPerception.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from  scipy.cluster.vq import whiten, kmeans, book
 class CavPerception:
     
     radarData = None #2d numpy array where each row represents a radar point in the form [velocity, azimuthAngle, altitude, depth]
@@ -12,6 +12,11 @@ class CavPerception:
     
     def __init__(self):
         pass
+
+    def clusterData(self,data,k):
+        clusters, _ = kmeans(data,k)
+        return clusters
+         #array  of objects found
     
     def processAndSetRadarData(self, rawRadarData): 
         self.radarData = np.reshape(rawRadarData, (-1, 4))
@@ -21,15 +26,34 @@ class CavPerception:
     
     def setEgoVehXVel(self, egoVehXVel_mps): self.egoVehXVel_mps = egoVehXVel_mps
     
-    def getLeadVehValidity(self): return self.leadVehValidity
+    def getLeadVehValidity(self): return self.leadVehValidity 
     def getLeadVehRelXPos_m(self): return self.leadVehRelXPos_m
     def getLeadVehRelXVel_mps(self): return self.leadVehRelXVel_mps
     
+    #Accepts clustered data
+    def findLeadCar(self,objects):
+            RADIUS =20 #radius of the cars sensors in (m)
+
+            #if lead car is directly infront of us and at least 20 meters away
+            for i in objects:
+                 if i[2]<RADIUS and i[1]<=95 and i[1]>=85:
+                      return i
+            return None
+
     def processAndFuseSensorData(self):
         
+        TIME_STEP=0.05
+        k=2
         # WRITE AN ALGORITHM THAT USES THE RADAR DATA (AND OPTIONALLY LIDAR DATA) TO DETERMINE THE POSITION AND VELOCITY OF THE LEAD VEHICLE (IF THERE IS ONE IN SENSOR RANGE)
         # IMLPEMENT YOUR CODE HERE! 
-        
-        self.leadVehValidity = False # validity flag (is there a lead vehicle in range?)
-        self.leadVehRelXPos_m = 0 # relative position of the lead vehicle wrt ego vehicle (meters)
-        self.leadVehRelXVel_mps = 0 # relative velocity of the lead vehicle wrt ego vehicle (meters/sec)
+        #detected objects
+        objects=self.clusterData(self.radarData,k)
+        leadCar=self.findLeadCar(objects)
+        if leadCar is not None:
+            self.leadVehValidity = True# validity flag (is there a lead vehicle in range?)
+            self.leadVehRelXPos_m =  leadCar[2]# relative position of the lead vehicle wrt ego vehicle (meters)
+            self.leadVehRelXVel_mps = leadCar[0]-self.egoVehXVel_mps # relative velocity of the lead vehicle wrt ego vehicle (meters/sec)
+        else:
+            self.leadVehValidity = False # validity flag (is there a lead vehicle in range?)
+            self.leadVehRelXPos_m = 0 # relative position of the lead vehicle wrt ego vehicle (meters)
+            self.leadVehRelXVel_mps = 0 # relative velocity of the lead vehicle wrt ego vehicle (meters/sec)
