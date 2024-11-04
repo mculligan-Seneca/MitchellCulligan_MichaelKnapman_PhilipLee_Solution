49c5058543510449bde85c1b48975d2989d5b02a
michaelknapman <70975021+michaelknapman@users.noreply.github.com>
2024-11-03 21:26:07 -0500

    i dont remember these changes
    i dont remember these changes but ill push them anyways. they are from like a year ago

--- cav-autera-software/cav-perception/CavPerception.py
+++ cav-autera-software/cav-perception/CavPerception.py
@@ -1,3 +1,43 @@
+
+import numpy as np
+
+class CavPerception:
+    
+    radarData = None #2d numpy array where each row represents a radar point in the form [velocity, azimuthAngle, altitude, depth]
+    lidarData = None #2d numpy array where each row represents a point of the point cloud in the form [xPosition, yPosition, zPosition]
+    
+    leadVehValidity = False
+    leadVehRelXPos_m = 0
+    leadVehRelXVel_mps = 0
+    egoVehXVel_mps = 0
+    
+    def __init__(self):
+        pass
+    
+    def processAndSetRadarData(self, rawRadarData): 
+        self.radarData = np.reshape(rawRadarData, (-1, 4))
+        
+    def processAndSetLidarData(self, rawLidarData): 
+        self.lidarData = np.reshape(rawLidarData, (-1, 3))
+    
+    def setEgoVehXVel(self, egoVehXVel_mps): self.egoVehXVel_mps = egoVehXVel_mps
+    
+    def getLeadVehValidity(self): return self.leadVehValidity
+    def getLeadVehRelXPos_m(self): return self.leadVehRelXPos_m
+    def getLeadVehRelXVel_mps(self): return self.leadVehRelXVel_mps
+    
+    def processAndFuseSensorData(self):
+        
+        # WRITE AN ALGORITHM THAT USES THE RADAR DATA (AND OPTIONALLY LIDAR DATA) TO DETERMINE THE POSITION AND VELOCITY OF THE LEAD VEHICLE (IF THERE IS ONE IN SENSOR RANGE)
+        # IMLPEMENT YOUR CODE HERE! 
+        
+        self.leadVehValidity = False # validity flag (is there a lead vehicle in range?)
+        self.leadVehRelXPos_m = 0 # relative position of the lead vehicle wrt ego vehicle (meters)
+        self.leadVehRelXVel_mps = 0 # relative velocity of the lead vehicle wrt ego vehicle (meters/sec)
+
+'''
+
+
 import numpy as np
 from  scipy.cluster.vq import kmeans,whiten
 from sklearn.cluster  import DBSCAN
@@ -48,7 +88,8 @@ class CavPerception:
   
 
     def processAndFuseSensorData(self):
-        
+
+
         TIME_STEP=0.05
         k=2
         # WRITE AN ALGORITHM THAT USES THE RADAR DATA (AND OPTIONALLY LIDAR DATA) TO DETERMINE THE POSITION AND VELOCITY OF THE LEAD VEHICLE (IF THERE IS ONE IN SENSOR RANGE)
@@ -66,4 +107,5 @@ class CavPerception:
                 self.leadVehValidity = False # validity flag (is there a lead vehicle in range?)
                 self.leadVehRelXPos_m = 0 # relative position of the lead vehicle wrt ego vehicle (meters)
                 self.leadVehRelXVel_mps = 0 # relative velocity of the lead vehicle wrt ego vehicle (meters/sec)
-               
+
+'''
